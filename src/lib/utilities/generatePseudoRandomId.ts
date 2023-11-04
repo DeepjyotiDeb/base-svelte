f76aa70262040c161e3a83a37a070e851a4b3903
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2023-11-04 10:35:32 +0530

    feat: testing server actions

---
+++ src/lib/utilities/generatePseudoRandomId.ts
@@ -0,0 +1,11 @@
+export function generatePseudoRandomId(characterLength: number) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+
+	for (let i = 0; i < characterLength; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		result += characters.charAt(randomIndex);
+	}
+
+	return result;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
+<script lang="ts">
+	import {enhance} from '$app/forms';
+
+  const authorizedExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
+</script>
+
 <p class="text-3xl font-bold underline">
   Hello world!
 </p>
 
 <h1>Hello and welcome to my site!</h1>
 <a href="/about" class="underline hover:text-blue-500">About my site</a>
+
+<form method="post" use:enhance enctype="multipart/form-data" class="flex flex-col" on>
+  <div class="form-control w-full max-w-xs">
+    <label class="label" for="pick">
+      <span class="label-text" id="pick">Pick a file</span>
+    </label>
+    <input type="file" class="file-input file-input-bordered w-full max-w-xs" accept={authorizedExtensions.join(',')}/>
+  </div>
+
+  <button type="submit" class="btn mx-auto">Submit</button>
+</form>
 
 <style lang="postcss">
    :global(html) {
