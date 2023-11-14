3ce5623a4c5f197486afa26dde60cae2874283ef
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2023-11-14 12:43:36 +0530

    feat: copy to clip board added

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -38,11 +38,6 @@
 		}, 3000);
 	};
 
-	// if (data.dbRes?.TTL) {
-	// 	console.log('time', data.dbRes.TTL, new Date().getTime() + 1 * 60 * 1000)
-	// 	objectExpired = data.dbRes.TTL > new Date().getTime() + 1 * 60 * 1000 ? false : true;
-	// }
-	// const fileName = extractFileNameFromUrl(data?.PresignedUrl || 'not-found')
 	const deleteItem = async (Id: string) => {
 		try {
 			const res = await fetch('api/delete', {
@@ -100,6 +95,7 @@
 			<div class="my-1">
 				<button class="btn" on:click={downloadAll}>Download All</button>
 				<button class="btn" on:click={triggerModal}>Delete All</button>
+				<button class="btn" on:click={copyToClipboard}>Copy URL</button>
 			</div>
 			<div class="flex flex-wrap md:gap-4 gap-2">
 				{#each files as file}
@@ -144,85 +140,6 @@
 			</div>
 		{/await}
 	{/if}
-	<!--
-	<div class="card card-compact w-96 bg-base-100 shadow-xl">
-		<figure>
-			<img src={data.dbRes && data.dbRes[0].DownloadUrl} alt="user-obj" class="max-w-xs h-[10vh]" />
-		</figure>
-		<div class="card-body">
-			<h2 class="card-title">Shoes!</h2>
-			<p>If a dog chews shoes whose shoes does he choose?</p>
-			<div class="card-actions justify-end">
-				<button class="btn btn-primary">Buy Now</button>
-			</div>
-		</div>
-	</div>
-	{#if !data?.dbRes?.length}
-		<div class="text-2xl text-center">No data found... Please check the url</div>
-	{:else if objectExpired}
-		<p>Object has expired, please upload again</p>
-	{:else}
-		{#await data?.dbRes then value}
-			<div class="flex justify-center gap-2">
-				<button type="button" class="btn" on:click={copyToClipboard}
-					>Copy Link
-					{#if isCopied}
-						<i class="fa-solid fa-check text-xl ml-1" />
-					{:else}
-						<i class="fa-solid fa-copy text-xl ml-1" />
-					{/if}
-				</button>
-				<button type="button" class="btn" on:click={deleteAll}>delete all</button>
-				<button class="btn m-auto block mt-4" on:click={downloadAll}
-					>Download All
-					<i class="fa-solid fa-download text-xl" />
-				</button>
-			</div>
-			{#each data?.dbRes as value}
-				<div class="flex justify-center items-center gap-4">
-					{#if value?.ContentType === 'image/jpeg'}
-						<img alt="user-upload" src={value?.DownloadUrl} class="h-[75vh] w-auto" />
-					{/if}
-					<div>
-						<a href={value?.ViewUrl} class="">
-							<button class="btn m-auto block mt-4"
-								>Download
-								<i class="fa-solid fa-download text-xl" />
-							</button>
-						</a>
-						<div class="text-center">Content Type : {value?.ContentType}</div>
-					</div>
-				</div>
-				<p>File: {value?.filename}</p>
-				<button type="button" class="btn" on:click={() => deleteItem(value?.Id)}>delete</button>
-			{/each}
-		{:catch error}
-			{error.message}
-		{/await}
-	{/if} -->
+
 	<DeleteModal {deleteAll} />
 </div>
-<!--
-{#if dbRes}
-	{#if dbRes.ContentType.includes('image')}
-		<img alt="user-media" src={dbRes?.PresignedUrl} />
-	{/if}
-	{#if dbRes.ContentType.includes('pdf')}
-		<object
-			data={dbRes?.PresignedUrl}
-			type="application/pdf"
-			title={fileName}
-		>
-			<p>unable to display file</p>
-		</object>
-	{/if}
-	{#if dbRes.ContentType.includes('video')}
-		<video controls width="250">
-			<source src={dbRes.PresignedUrl} type="video/webm" />
-			<track kind="captions" />
-		</video>
-	{/if}
-	<button type="button" class="button">
-		<a href={dbRes.PresignedUrl} download={fileName}> download </a>
-	</button>
-{/if} -->
